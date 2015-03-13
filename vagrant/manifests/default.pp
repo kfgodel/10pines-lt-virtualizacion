10fbbb4889ea9d62b88113863c71434234db6e9c
Dario L Garcia <kfgodel@yahoo.com>
2015-03-12 21:26:25 -0300

    Installed Java 8

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -1,2 +1,33 @@
 # Print puppet version
 notify { "Puppet: ${puppetversion}": }
+
+# Install Oracle Java 8
+class oracleJava {
+
+    $jdk_version = '8'
+
+    file { '/tmp/java.preseed':
+        content => "oracle-java${jdk_version}-installer shared/accepted-oracle-license-v1-1 select true",
+        mode   => '0600',
+        backup => false,
+    }
+
+
+    include apt
+
+    apt::ppa { 'ppa:webupd8team/java': }
+
+    package { "oracle-java${jdk_version}-installer":
+        responsefile => '/tmp/java.preseed',
+        require      => [
+                        Apt::Ppa['ppa:webupd8team/java'],
+                        File['/tmp/java.preseed']
+                        ],
+    }
+
+    package { "oracle-java${jdk_version}-set-default":
+        require => Package["oracle-java${jdk_version}-installer"]
+    }
+}
+
+include oracleJava
